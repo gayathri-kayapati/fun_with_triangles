aaf08aacc915ec75cc78150ca6abab843b8c9198
gayathri-kayapati <gayathrikayapati@gmail.com>
2023-01-25 15:53:55 +0530

    js:added quiz questions and answers

--- quiz.js
+++ quiz.js
@@ -1,13 +1,114 @@
 var questions = [
   {
-    question: "",
+    question: "1. What is the name of the triangle with no equal sides?",
     options: [
       {
-        option1: "",
-        option2: "",
-        option3: "",
+        option1: "Scalene",
+        option2: "Caitlyn",
+        option3: "Galen",
       },
     ],
-    answer: "",
+    answer: "Scalene",
+  },
+  {
+    question: "2. How many dimensions does have a triangle?",
+    options: [
+      {
+        option1: "4",
+        option2: "3",
+        option3: "2",
+      },
+    ],
+    answer: "2",
+  },
+  {
+    question: "3. What is the sum of the anglesin the triangle?",
+    options: [
+      {
+        option1: "270 degrees",
+        option2: "180 degrees",
+        option3: "90 degrees",
+      },
+    ],
+    answer: "180 degrees",
+  },
+  {
+    question:
+      "4. The side opposite to the right angle in right angled triangle is called? ",
+    options: [
+      {
+        option1: "Base",
+        option2: "Perpendicular",
+        option3: "Hypotenuse",
+      },
+    ],
+    answer: "Hypotenuse",
+  },
+  {
+    question: "5. The perimeter of scalene triangle with the sides a,b,c is?",
+    options: [
+      {
+        option1: "a+b+c",
+        option3: "2a",
+        option3: "None of these",
+      },
+    ],
+    answer: "a+b+c",
+  },
+  {
+    question: "6. Which of the set of three sides can't form a triangle?",
+    options: [
+      {
+        option1: "5cm,6cm,7cm",
+        option2: "5cm,8cm,15cm",
+        option3: "8cm,15cm,18cm",
+      },
+    ],
+    answer: "5cm,8cm,15cm",
+  },
+  {
+    question: "7. A triangle with all angles are less than 90 degrees?",
+    options: [
+      {
+        option1: "Acute",
+        option2: "Obtuse",
+        option3: "Equilateral",
+      },
+    ],
+    answer: "Acute",
+  },
+  {
+    question: "8. Which center is equidistant from the vertices of a triangle?",
+    options: [
+      {
+        option1: "Incenter",
+        option2: "Centroid",
+        option3: "The circumcenter",
+      },
+    ],
+    answer: "The circumcenter",
+  },
+  {
+    question: "9. The correct formula to find the area of trinagle is?",
+    options: [
+      {
+        option1: "½ × base × height",
+        option2: "½ × base + height",
+        option3: "½ + base + height",
+      },
+    ],
+    answer: "½ × base × height",
+  },
+  {
+    question:
+      "10. What's the name of the type of maths that studies the sides and angles of triangles?",
+    options: [
+      {
+        option1: "Geometery",
+        option2: "Trigonometry",
+        option3: "Algebra",
+      },
+    ],
+    answer: "Trigonometry",
   },
 ];
